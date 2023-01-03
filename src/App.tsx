23df7fe8ccb95c9ded65e23ffe3ccbfdb0738a5a
Robin Panta <hacktivistic@gmail.com>
2023-01-04 02:24:17 +0530

    get and log file array buffer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { useRef } from "react";
 
 function App() {
-  // const fileInputRef = useRef();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleFile = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (!fileInputRef) return alert("Unknown Error Occurred!");
+
+    const fileItem = fileInputRef.current?.files?.item(0);
+    if (!fileItem) return alert("No any valid file selected.");
+
+    // Initial Test, checking from file.type
+    if (fileItem.type != "image/bmp") return alert("Not a valid BMP file.");
+
+    // Reading File
+    const reader = new FileReader();
+    reader.onload = () => {
+      console.log(reader.result);
+    };
+
+    reader.readAsArrayBuffer(fileItem);
+  };
 
   return (
     <div className="App">
       <h1>Bitmap Parser</h1>
       <div className="card">
-        <input type="file" />
-        <button>Parse Bitmap</button>
+        <input type="file" ref={fileInputRef} />
+        <button onClick={(e) => handleFile(e)}>Parse Bitmap</button>
         <p>
           Upload a <code>Bitmap (*.bmp)</code> to see the bitmap drawn in the
           canvas below after being parsed.
